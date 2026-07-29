3ed65e98688956d232990b69bf9991bd9b239e5d
Lauren <l.corbieee@gmail.com>
2026-07-29 16:17:55 -0500

    Update CHI-311-streamlit-app.py

--- CHI-311-streamlit-app.py
+++ CHI-311-streamlit-app.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd
 
 # home page formatting
-st.title("CHI 311 Data Snapshot: 2019-2025\n")
-st.markdown("Powered by UChicago DSSI")
+title = st.title("CHI 311 Data Snapshot: 2019-2025\n")
+title_mark = st.markdown("Powered by the City of Chicago and UChicago DSSI")
+
 # sidebar formatting
+tab1, tab2, tab3, tab4 = st.tabs(["Home", "Dataset Breakdown", "Geospatial Analysis", "Forecasting"])
 st.sidebar.write("Select how you want to interpret our data:")
-st.sidebar.button("Home")
-st.sidebar.button("Dataset Breakdown")
-st.sidebar.button("Geospatial analysis")
-st.sidebar.button("Forecasting call volume by Chicago neighborhood")
