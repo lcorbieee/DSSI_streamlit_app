08c714481dbff3d9ba3bcb3043d4c1bf736f5d60
Lauren <l.corbieee@gmail.com>
2026-07-29 16:42:43 -0500

    Update CHI-311-streamlit-app.py

--- CHI-311-streamlit-app.py
+++ CHI-311-streamlit-app.py
@@ -5,6 +5,17 @@ import pandas as pd
 title = st.title("Forecasting 311 Call Volume by Chicago Neighborhood\n")
 title_mark = st.markdown("Powered by UChicago Data Science for Social Impact and the City of Chicago")
 
-# sidebar formatting
+# tab formatting
 tab_forecast, tab_geospastial, tab_snapshot, tab_breakdown = st.container(border=True).tabs(["Forecasting 🔮", "Geospatial Analysis 🗺️", "Snapshots 📸", "Dataset Breakdown 🧐"])
 
+# tab_forecast formatting
+with tab_forecast:
+
+# tab_geospastial formatting
+with tab_geospastial:
+
+# tab_snapshot formatting
+with tab_snapshot:
+
+# tab_breakdown formatting
+with tab_breakdown:
