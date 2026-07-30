1dd51d4d3e2b1a519d6e878fe269685d31106e1d
Lauren <l.corbieee@gmail.com>
2026-07-30 13:47:29 -0500

    Update CHI-311-streamlit-app.py

--- CHI-311-streamlit-app.py
+++ CHI-311-streamlit-app.py
@@ -9,13 +9,14 @@ title_mark = st.markdown("Powered by UChicago Data Science for Social Impact and
 tab_forecast, tab_geospastial, tab_snapshot, tab_breakdown = st.container(border=True).tabs(["Forecasting 🔮", "Geospatial Analysis 🗺️", "Snapshots 📸", "Dataset Breakdown 🧐"])
 
 # tab_forecast formatting
-# with tab_forecast:
-#     st.
-# # tab_geospastial formatting
-# with tab_geospastial:
-
-# # tab_snapshot formatting
-# with tab_snapshot:
-
-# # tab_breakdown formatting
-# with tab_breakdown:
+with tab_forecast:
+    st.subheader("Models were trained on monthly Chicago 311 calls from 2019-2025.")
+# tab_geospastial formatting
+with tab_geospastial:
+    st.subheader("Below is an interactive map of Chicago's 77 community areas.  Hover and see how different neighborhoods use 311.")
+# tab_snapshot formatting
+with tab_snapshot:
+    st.subheader("Enjoy some bite-sized analyses of our data.")
+# tab_breakdown formatting
+with tab_breakdown:
+    st.subheader("Learn more about our dataset and research methods.")
