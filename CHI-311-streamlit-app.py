bff935f21e11461899af345ac41e53f50a80e0e6
Lauren <l.corbieee@gmail.com>
2026-07-30 14:01:05 -0500

    Update CHI-311-streamlit-app.py

--- CHI-311-streamlit-app.py
+++ CHI-311-streamlit-app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 import pandas as pd
 
+@st.cache_data
+def load_entire_df():
+    return pd.read_csv("data/request_counts_by_community_area_year_month.csv")
+
+@st.cache_data
+def load_categories():
+    return pd.read_csv("data/request_type_categories.csv")
+
+@st.cache_data
+def load_response_times():
+    return pd.read_csv("data/median_response_time.csv")
+
+@st.cache_data
+def load_forecasts():
+    return pd.read_csv("data/citywide_forecast_results.csv", parse_dates=["date"])
+
+total_requests = load_entire_df()
+categories = load_categories()
+response_times = load_response_times()
+forecasts = load_forecasts()
+
 # home page formatting
 title = st.title("Forecasting 311 Call Volume by Chicago Neighborhood\n")
 title_mark = st.markdown("Powered by UChicago Data Science for Social Impact and the City of Chicago")
@@ -17,6 +38,7 @@ with tab_geospastial:
 # tab_snapshot formatting
 with tab_snapshot:
     st.subheader("Enjoy some bite-sized analyses of our data.")
+    st.dataframe(categories, use_container_width=True)
 # tab_breakdown formatting
 with tab_breakdown:
     st.subheader("Learn more about our dataset and research methods.")
